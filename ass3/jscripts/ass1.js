918684e6464b03a678bee4a512297e28c729c626
kusumalatha2000 <65996219+kusumalatha2000@users.noreply.github.com>
2020-07-16 11:09:43 +0530

    Add files via upload

---
+++ ass3/jscripts/ass1.js
@@ -0,0 +1,11 @@
+alert("Enter the user details");
+let name = prompt("Enter the name");
+let age = prompt("Enter the age");
+let city = prompt("Enter city");
+let phonenumber = prompt("enter mobile number");
+confirm("Save all the details");
+console.log("name : " + name);
+console.log("age : " + age);
+console.log("city : " + city);
+console.log("phonenumber: " + phonenumber);
+
