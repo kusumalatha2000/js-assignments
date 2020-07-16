8eb5f79741ea6b6d15e4ae2d81dfacc429691e31
kusumalatha2000 <65996219+kusumalatha2000@users.noreply.github.com>
2020-07-16 11:10:16 +0530

    Add files via upload

---
+++ ass3/jscripts/ass2.js
@@ -0,0 +1,40 @@
+let str = "LetsUpgrade is a good coding school";
+let str1 = " And is a best platform";
+console.log(str);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.length);
+console.log(str[4]);
+console.log(str.indexOf('U'));
+console.log(str.lastIndexOf('d'));
+console.log(str.charAt(5));
+console.log(str.substring(2,6));
+console.log(str.startsWith('is'));
+console.log(str.endsWith("school"));
+console.log(str.slice(1,4));
+console.log(str.split(" "));
+console.log(str.search("i"));
+console.log(str.replace("is","was"));
+console.log(str + str1);
+let arr1 = ["1",2,"app","bat"];
+let arr2 = ["0","cat"];
+console.log(arr1);
+console.log(arr2);
+console.log(Array.isArray(arr1));
+console.log(arr1.reverse());
+console.log(arr1.concat(arr2));
+arr1.unshift("5");
+console.log(arr1);
+arr2.push("doll");
+console.log(arr2);
+arr1.pop()
+console.log(arr1);
+arr1.shift();
+console.log(arr1);
+console.log(arr1.length);
+arr1.splice(2,0,"cool","too1");
+console.log(arr1);
+arr1.sort();
+console.log(arr1);
+
+
