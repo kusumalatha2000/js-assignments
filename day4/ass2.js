2ef772e1568dcdf8169e312792f74c6234d0c0b2
kusumalatha2000 <65996219+kusumalatha2000@users.noreply.github.com>
2020-07-19 20:13:25 +0530

    js file

---
+++ day4/ass2.js
@@ -0,0 +1,9 @@
+const student = {
+    name : "Helsinki",
+    age : 24,
+    projects : {
+        diceGame : "Two player dice game using JavaScript"
+    }
+}
+const {name,age,projects : {diceGame}} = student;
+console.log(name,age,diceGame);
