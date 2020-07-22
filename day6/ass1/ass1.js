3f7a860523b734ce3b7d8e855e706074f967768c
kusumalatha2000 <65996219+kusumalatha2000@users.noreply.github.com>
2020-07-22 13:52:45 +0530

    Add files via upload

---
+++ day6/ass1/ass1.js
@@ -0,0 +1,12 @@
+var doc = document.getElementById("background");
+var color = ["black", "blue", "brown", "green"];
+var i = 0;
+function change() {
+  doc.style.backgroundColor = color[i];
+  i++;
+  
+  if(i > color.length - 1) {
+    i = 0;
+  }
+}
+setInterval(change, 1000);
